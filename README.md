c55ee7aa35333c911300319b2ea0ac52f742c052
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2013-01-12 21:24:09 +0200

    Made the program and readme more verbose.

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Kauko is remote control for a computer.
 ![LOGO](https://github.com/kimmobrunfeldt/kauko/raw/master/static/img/kauko.png)
 
 Client runs in browser, so almost any device can be used as a remote control. Computer runs a webserver written in Python.
+
+Requirements
+------------
+
+- Python ( http://www.python.org/ )
+- gevent ( http://www.gevent.org/ )
+
+Make it run
+-----------
+
+Note that the program needs super user privileges because it runs a web server on port 80.
+
+1. Install gevent.
+
+    pip install gevent
+
+2. Open terminal and run main.py
+
+    sudo ./main.py
+
+3. Browse to your computer's ip address with your browser. main.py prints your ip address but it might not be correct.
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WARNING: Do NOT use this as a public web server in any way, this is only for
 """
 
 import logging
+import socket
 import sys
 
 from gevent import pywsgi
@@ -13,24 +14,28 @@ from gevent import pywsgi
 import wsgiserver
 
 
-def setup_logging(root_logger):
+def setup_logging(root_logger, level=logging.DEBUG):
     format = '%(asctime)-15s %(name)-15s %(levelname)-8s %(message)s'
     formatter = logging.Formatter(format)
     root_logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(level)
     root_logger.addHandler(console_handler)
 
 
 def main():
-    setup_logging(logging.getLogger(''))
+    setup_logging(logging.getLogger(''), level=logging.ERROR)
+
+    print('\nKauko is now started, quit the program by pressing Ctrl - C')
+    print('\nOpen the following address in your browser:')
+    print('http://%s' % socket.gethostbyname(socket.gethostname()))
+
 
     # Serve static files and routes with wsgi app
     http_server = pywsgi.WSGIServer(('0.0.0.0', 80), wsgiserver.main_app)
     http_server.serve_forever()
-
 
 if __name__ == '__main__':
     main()
