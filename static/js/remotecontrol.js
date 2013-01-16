dfc4879502ddd1d872a43b9f03357f5509f9b07e
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2013-01-17 01:07:18 +0200

    Added functionality to dynamically create button grid.

--- static/js/remotecontrol.js
+++ static/js/remotecontrol.js
@@ -13,10 +13,11 @@ var RemoteControl = (function(options) {
 
         Looks like:
         [
-            [Button, Button, Button],
+            [{}, null, {}],
                 ...
-            [Button, Button, Button]
+            [{}, {}, {}]
         ]
+        Where each non-null element is the options to initialize Button with.
 
     options.defaultClasses:
         The default CSS classes that are added for a button.
@@ -24,29 +25,111 @@ var RemoteControl = (function(options) {
     */
 
     var defaultOptions = {
-
+        buttonEvent: 'pointerdown',
+        commandErrorCallback: function() { location.reload(); }
     };
 
+    options = $.extend(true, {}, defaultOptions, options);
+
     var my = {},
-        options = $.extend(true, {}, defaultOptions, options);
+        buttons,
+        element = options.element;
+
 
     function init() {
-
+        buttons = initializeButtons(options.buttons);
+        my.initializeAndBind();
     }
 
     //
     // Public methods
     //
 
-    my.draw = function() {
+    my.initializeAndBind = function() {
+        // Clear html from element.
+        element.html('');
 
+        for (var row = 0; row < buttons.length; ++row) {
+            element.append(rowToHTML(row));
+
+            for (var column = 0; column < buttons[0].length; ++column) {
+                var button = buttons[row][column];
+
+                if (button !== null && button.autoSend) {
+                    bindButtonToSendCommand(button);
+                }
+            }
+        }
+    };
+
+    // Sends command to server in format: [command, [param1, ..., paramN]]
+    // args and kwargs are optional.
+    my.sendCommand = function(command, args, kwargs) {
+        if (typeof args === 'undefined') {
+            args = [];
+        }
+
+        if (typeof kwargs === 'undefined') {
+            kwargs = {};
+        }
+
+        $.ajax({
+            url: "/command",
+            type: "POST",
+            data: JSON.stringify([command, args, kwargs]),
+            dataType: "json",
+            async: false,
+            error: function(xhr, textStatus, errorThrown) {
+                options.commandErrorCallback();
+            }
+        });
     };
 
     //
     // Private methods
     //
 
+    // Takes the button grid of initializing parameters and initializes them
+    // to Button instances and returns the new list.
+    function initializeButtons(buttons) {
+        var newButtons = [];
 
+        for (var row = 0; row < buttons.length; ++row) {
+            var newRow = [];
+            for (var column = 0; column < buttons[0].length; ++column) {
+                if (buttons[row][column] !== null) {
+                    newRow.push(new Button(buttons[row][column]));
+                } else {
+                    newRow.push(null);
+                }
+            }
+            newButtons.push(newRow);
+        }
+        return newButtons;
+    }
+
+    function rowToHTML(rowIndex) {
+        var html = '<div class="row-fluid">';
+        for (var column = 0; column < buttons[0].length; ++column) {
+            html += '<div class="span4">';
+
+            var button = buttons[rowIndex][column];
+            if (button !== null) {
+                html += button.toHTML();
+            }
+
+            html += '</div>';
+        }
+        html += '</div>';
+
+        return html;
+    }
+
+    function bindButtonToSendCommand(button) {
+        $('#' + button.id).on(options.buttonEvent, function(evt) {
+            my.sendCommand('button_press', [button.id]);
+        });
+    }
 
     init();
     return my;
@@ -64,6 +147,9 @@ var Button = (function(options) {
     */
 
     var defaultOptions = {
+        // If true, button press command is sent automatically on press
+        // with out manually binding any events
+        autoSend: true,
 
         // small, large
         size: "large",
@@ -73,7 +159,7 @@ var Button = (function(options) {
         // Name of the icon file in static/img/ directory.
         icon: "",
 
-        // Button label.
+        // Button label. The default text size assumes one letter labels.
         label: "",
 
         // What CSS classes to add along with 'btn' and the size class which
@@ -83,5 +169,27 @@ var Button = (function(options) {
     };
 
     var my = $.extend(true, {}, defaultOptions, options);
+
+    // Maybe using a template would be more clean.
+    my.toHTML = function() {
+
+        var classes = 'btn-' + my.size + ' ' + my.cssClasses;
+
+        // Start button tag
+        var html = '<button type="button" class="btn ' + classes + '" ';
+        html += 'id="' + my.id + '">';
+
+        // Add label for button
+        if (my.icon.length > 0) {
+            html += '<img src="/static/img/' + my.icon + '">';
+        } else {
+            html += my.label;
+        }
+
+        html += '</button>';
+
+        return html;
+    };
+
     return my;
 });
